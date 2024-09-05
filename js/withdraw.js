7cb21b4867727232aa10b11f0e112badf793a4db
tahiamustofa <tahiamustofa@gmail.com>
2024-09-05 14:18:30 +0600

    update commit4

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,6 +2,7 @@ document.getElementById('btn-withdraw').addEventListener('click',function( ) {
   const withdrawFeild = document.getElementById('withdrawFeild');
   const withdrawFeildValue = parseFloat(withdrawFeild.value);
 
+ 
   withdrawFeild.value='';
 
   if (isNaN (withdrawFeildValue)) {
@@ -12,16 +13,16 @@ document.getElementById('btn-withdraw').addEventListener('click',function( ) {
   const totalWithdraw= document.getElementById('totalWithdraw');
   const totalWithdrawValue = parseFloat(totalWithdraw.innerText);
 
+
+  const balanceEle= document.getElementById('balanceEle');
+  const balanceElePrev = parseFloat(balanceEle.innerText);
+
   if (withdrawFeildValue>balanceElePrev) {
     alert('etu taka nai')
     return;
   }
 
   totalWithdraw.innerText = totalWithdrawValue+withdrawFeildValue;
-
-  const balanceEle= document.getElementById('balanceEle');
-  const balanceElePrev = parseFloat(balanceEle.innerText);
-
 
 balanceEle.innerText = balanceElePrev - withdrawFeildValue;
 })
